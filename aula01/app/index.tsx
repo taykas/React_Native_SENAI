0a1c5fc52bdec35f81cd483725aa01ccc78c02ae
taykas <thaynaschaeffer@gmail.com>
2025-10-29 10:16:04 -0300

    Hello World!!

---
+++ aula01/app/index.tsx
@@ -0,0 +1,14 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/hello-wave';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { Link } from 'expo-router'; 
+
+export default function HomeScreen() {
+    return (
+        <h1>Hello World</h1>
+    );
+}
