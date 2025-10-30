e91f885c81852757364a2eeac344095f8173d273
taykas <thaynaschaeffer@gmail.com>
2025-10-30 11:05:29 -0300

    omg test

--- aula01/app/index.tsx
+++ aula01/app/index.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 const styles = StyleSheet.create({
     colorBlue: {
@@ -14,17 +15,30 @@ const styles = StyleSheet.create({
         width: 150,
         height: 150,
         backgroundColor: "purple"
+    },
+    image: {
+
     }
 })
 
 export default function HomeScreen() {
+    const [text, setText] = useState("")
+
     return (
         <>
-            <View>
-                <Text style={styles.colorBlue}>Hello World!</Text>
-            </View>
             <View style={styles.div}>
+                <Text style={styles.colorBlue}>Hello World!</Text>
                 <View style={styles.square}></View>
+                <TextInput placeholder='Digite aqui...' onChangeText={text => setText(text)}></TextInput>
+                <TouchableOpacity>
+                    <View>
+                        <Text>Press me</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <Button title="press me" onPress={() => console.log('oi')}></Button>
+
+                <Image style={{width: 300, height: 300}} source={require("../assets/images/cat.jpg")}/>
             </View>
         </>
     );
